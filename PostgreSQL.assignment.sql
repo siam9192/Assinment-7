5a23bb20cdc9d62bb3afd0e93c91f0dfac1ef054
siam9192 <ahsiam999@gmail.com>
2024-10-24 23:32:53 +0600

    Updated

--- PostgreSQL.assignment.sql
+++ PostgreSQL.assignment.sql
@@ -5,9 +5,13 @@
 CREATE DATABASE university_db;
 
 
+DROP TABLE enrollment;
+DROP TABLE students;
+DROP TABLE courses;
+
 -- Create students table
 CREATE TABLE students(
-student_id INT UNIQUE PRIMARY  KEY,
+student_id SERIAL PRIMARY KEY,
 student_name VARCHAR(50) NOT NULL,
 age INT NOT NULL,
 email VARCHAR(50) NOT NULL,
@@ -16,12 +20,36 @@ backend_mark INT,
 status VARCHAR(20)
 );
 
+
+
+
 -- Create course table
 CREATE TABLE courses (
-    course_id  INT UNIQUE PRIMARY KEY,
+    course_id  SERIAL PRIMARY KEY,
     course_name VARCHAR(200) NOT NULL,
     credits INT
 );
+
+
+
+-- Create enrollment table
+CREATE TABLE enrollment (
+    enrollment_id SERIAL PRIMARY KEY,
+    student_id INT REFERENCES students(student_id),
+    course_id INT REFERENCES courses(course_id)
+);
+
+
+-------------- Insert Data-------------
+
+-- Insert Students Data
+INSERT INTO students (student_id,student_name,age,email,frontend_mark,backend_mark,status)
+VALUES (1,'John Doe', 20, 'john.doe@example.com', 85, 90, NULL),
+(2,'Jane Smith', 22, 'jane.smith@example.com', 88, 82, NULL),
+(3,'Michael Johnson', 21, 'michael.johnson@example.com', 92, 87, NULL),
+(4,'Emily Davis', 19, 'emily.davis@example.com', 78, 80, NULL),
+(5,'Daniel Brown', 23, 'daniel.brown@example.com', 90, 85, NULL),
+(6,'Sophia Wilson', 20, 'sophia.wilson@example.com', 84, 89, NULL);
 
 -- Insert courses table data
 INSERT INTO courses (course_id,course_name, credits)
@@ -35,17 +63,10 @@ VALUES
 (7,'GraphQL', 4);
 
 
--- Create enrollments table
-CREATE TABLE enrollments (
-    enrollment_id INT UNIQUE PRIMARY KEY,
-    student_id INT REFERENCES students(student_id),
-    course_id INT REFERENCES courses(course_id)
-);
 
+-- Insert enrollment table  
 
--- Insert enrollments table values
-
-INSERT INTO enrollments (enrollment_id,student_id, course_id)
+INSERT INTO enrollment (enrollment_id,student_id, course_id)
 VALUES
 (1,1, 1),
 (2,1, 2),
@@ -61,17 +82,13 @@ VALUES
 ------ Query 1------
 -- Insert student data in students table
 INSERT INTO students (student_id,student_name,age,email,frontend_mark,backend_mark,status)
-VALUES (1,'John Doe', 20, 'john.doe@example.com', 85, 90, NULL),
-(2,'Jane Smith', 22, 'jane.smith@example.com', 88, 82, NULL),
-(3,'Michael Johnson', 21, 'michael.johnson@example.com', 92, 87, NULL),
-(4,'Emily Davis', 19, 'emily.davis@example.com', 78, 80, NULL),
-(5,'Daniel Brown', 23, 'daniel.brown@example.com', 90, 85, NULL),
-(6,'Sophia Wilson', 20, 'sophia.wilson@example.com', 84, 89, NULL);
+VALUES (9,'John Doe', 20, 'john.doe@example.com', 85, 90, NULL);
+
 
 
 ------ Query 2------
 -- Retrieve the names of all students who are enrolled in the course titled 'Next.js'.
-SELECT student_name FROM (SELECT * FROM enrollments as e
+SELECT student_name FROM (SELECT * FROM enrollment as e
 INNER JOIN students as s ON s.student_id = e.student_id
 INNER JOIN courses as c ON c.course_id = e.course_id) WHERE course_name = 'Next.js';
 
@@ -89,9 +106,11 @@ INNER JOIN courses as c ON c.course_id = e.course_id) WHERE course_name = 'Next.
 
 ------ Query 4------
 -- Delete all courses that have no students enrolled.
-DELETE FROM courses WHERE course_id NOT IN (SELECT course_id FROM enrollments
+DELETE FROM courses WHERE course_id NOT IN (SELECT course_id FROM enrollment
 GROUP BY course_id)
 ;
+
+
 
 ------ Query 5------
 -- Retrieve the names of students using a limit of 2, starting from the 3rd student.
@@ -102,7 +121,7 @@ SELECT student_name,student_id FROM students  student_id LIMIT 2 OFFSET 2 ;
 -- Retrieve the course names and the number of students enrolled in each course.
 SELECT course_name,COUNT(student_id) AS students_enrolled 
 FROM courses c
-LEFT JOIN enrollments e ON c.course_id = e.course_id
+LEFT JOIN enrollment e ON c.course_id = e.course_id
 GROUP BY c.course_id,course_name ORDER BY  students_enrolled DESC;
 
 
